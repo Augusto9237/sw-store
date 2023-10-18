2317f883a151fdee4189885f4b3366af73b7785d
Augusto9237 <augusto.souza8330@gmail.com>
2023-10-17 23:20:53 -0300

    chore : add next auth setup

---
+++ src/lib/prisma.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+    var cachedPrisma: PrismaClient;
+}
+
+let prisma: PrismaClient;
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient()
+} else {
+    if (!globalThis.cachedPrisma) {
+        globalThis.cachedPrisma = new PrismaClient();
+    }
+    prisma = globalThis.cachedPrisma;
+}
+
+export const prismaClient = prisma
