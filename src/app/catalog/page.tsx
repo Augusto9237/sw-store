f7d1217afbae1ccb9ead5f9b67cb6672256ed706
Augusto9237 <augusto.souza8330@gmail.com>
2023-10-19 02:00:16 -0300

    Added a "Catalogo" badge to the catalog page

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,5 +1,18 @@
+import { Badge } from "@/components/ui/badge";
+import { ShapesIcon } from "lucide-react";
+
 const CatalogPage = () => {
-    return <h1>catalog</h1>;
+    return (
+        <div className="p-5">
+            <Badge
+                className="gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+                variant='outline'
+            >
+                <ShapesIcon size={16} />
+                Catalogo
+            </Badge>
+        </div>
+    )
 }
- 
+
 export default CatalogPage;
