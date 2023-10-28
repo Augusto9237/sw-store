584331b3308cc93b7fdbaba754b65f1495f90d8c
Augusto9237 <augusto.souza8330@gmail.com>
2023-10-28 20:09:18 -0300

    feat: The diff removes the `md:hidden` class from the `::-webkit-scrollbar` element, which will make the scrollbar visible on mobile devices

--- src/components/ui/product-list.tsx
+++ src/components/ui/product-list.tsx
@@ -7,7 +7,7 @@ interface ProductListProps {
 }
 export default function ProductList({ products }: ProductListProps) {
     return (
-        <div className="flex w-full gap-4 md:gap-8 overflow-x-auto px-5 md:px-0 [&::-webkit-scrollbar]:md:hidden">
+        <div className="flex w-full gap-4 md:gap-8 overflow-x-auto px-5 md:px-0 [&::-webkit-scrollbar]:hidden">
             {products.map(product => (
                 <div key={product.id} className="w-[170px] max-w-[170px]">
                     <ProductItem product={computeProductTotalPrice(product)} />
