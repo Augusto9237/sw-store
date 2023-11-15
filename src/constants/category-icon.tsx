404184dbe0ae8a740994ae9ffcaa291fac583be1
Augusto9237 <augusto.souza8330@gmail.com>
2023-11-15 00:50:21 -0300

    update

--- src/constants/category-icon.tsx
+++ src/constants/category-icon.tsx
@@ -1,11 +1,15 @@
-import { KeyboardIcon, MonitorIcon, HeadphonesIcon, SquareIcon, SpeakerIcon, MouseIcon, Gamepad2 } from "lucide-react";
+import { KeyboardIcon, MonitorIcon, HeadphonesIcon, SquareIcon, SpeakerIcon, MouseIcon, Gamepad2, CableIcon, ComponentIcon, GamepadIcon, LaptopIcon, PowerIcon, PrinterIcon } from "lucide-react";
 
 export const CATEGORY_ICON = {
-    keyboards: <KeyboardIcon size={16} />,
+    laptops: <LaptopIcon size={16} />,
     monitors: <MonitorIcon size={16} />,
+    keyboards: <KeyboardIcon size={16} />,
+    mice: <MouseIcon size={16} />,
     headphones: <HeadphonesIcon size={16} />,
-    mousepads: <SquareIcon size={16} />,
     speakers: <SpeakerIcon size={16} />,
-    mouses: <MouseIcon size={16} />,
-    consoles: <Gamepad2 size={16}/>
-}
+    printers: <PrinterIcon size={16} />,
+    gaming: <GamepadIcon size={16} />,
+    components: <ComponentIcon size={16} />,
+    cables: <CableIcon size={16} />,
+    power: <PowerIcon size={16} />,
+};
