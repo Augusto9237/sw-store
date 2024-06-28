712c59db16a16d4db8298f7d84078d844c6cad80
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2024-06-28 19:05:59 +0000

    feat: update page dashboard

--- src/app/(admin)/dashboard/components/card-total-orders.tsx
+++ src/app/(admin)/dashboard/components/card-total-orders.tsx
@@ -8,7 +8,7 @@ import { useContext } from "react";
 export default function CardTotalOrders() {
     const { orders } = useContext(AdminContext)
     return (
-        <Card className="flex flex-col h-32 p-5 gap-2">
+        <Card className="flex flex-col p-5 gap-2">
             <span className='opacity-75 max-md:text-sm'>Total de pedidos</span>
             <div className='flex justify-between items-center'>
                 <h1 className='text-4xl max-md:text-2xl max-sm:text-base font-bold'>{orders.length}</h1>
