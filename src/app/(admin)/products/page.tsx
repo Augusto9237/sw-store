250cff08965c7105c56fe3234f5ae9dacdf46293
Augusto Sousa <79643279+Augusto9237@users.noreply.github.com>
2024-07-01 17:27:55 +0000

    feat: update page products

--- src/app/(admin)/products/page.tsx
+++ src/app/(admin)/products/page.tsx
@@ -4,9 +4,9 @@ import CategoriesList from "./components/category-list"
 export default async function Products() {
 
   return (
-    <div className="flex w-full max-lg:flex-col  h-full gap-8 max-sm:gap-4 max-md:mb-4">
+    <div className="flex w-full max-lg:flex-col  h-full gap-8 max-sm:gap-4">
       <ProductList />
-      
+
       <div className="max-md:pb-4 lg:max-w-[355px] w-full">
         <CategoriesList />
       </div>
