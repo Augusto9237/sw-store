f483f3289f07382397174610d9719a406d66e013
Augusto9237 <augusto.souza8330@gmail.com>
2024-07-15 23:48:47 -0300

    feat: update stripe

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "fsw-store.s3.sa-east-1.amazonaws.com",
-      "bktdbogoranfqtptzqgs.supabase.co",
-    ],
     remotePatterns: [
       {
+        protocol: "https",
         hostname: "sw-store-images.s3.sa-east-1.amazonaws.com",
+        pathname: "**"
       },
     ],
   },
--- src/actions/checkout.ts
+++ src/actions/checkout.ts
@@ -8,7 +8,7 @@ export const createCheckout = async (
     orderId: string,
 ) => {
     const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-        apiVersion: "2023-10-16",
+        apiVersion: "2024-06-20",
     });
 
     const checkout = await stripe.checkout.sessions.create({
