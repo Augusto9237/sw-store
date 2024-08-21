ebe124353ebb5eb687663a366b23c99ce7e71169
Augusto9237 <augusto.souza8330@gmail.com>
2024-08-21 00:44:45 -0300

    feat: update page dashboard and add card total sales day component

---
+++ src/app/(admin)/dashboard/components/card-totalsales-day.tsx
@@ -0,0 +1,33 @@
+"use client"
+import { Card } from '@/components/ui/card'
+import { formatReal } from '@/helpers/formatReal';
+import { TotalSumOrders } from '@/helpers/order';
+import { AdminContext } from '@/providers/admin';
+import { CircleDollarSign } from 'lucide-react'
+import { useContext } from 'react';
+
+
+export default function CardTotalSalesDay() {
+    const { orders } = useContext(AdminContext)
+
+    const filterOrdersByToday = () => {
+        const today = new Date().toISOString().slice(0, 10); // Formata a data atual como 'YYYY-MM-DD'
+
+        return orders.filter(order => new Date(order.createdAt).toISOString().slice(0, 10) === today);
+    }
+
+    const totalOrdersDay = filterOrdersByToday()
+
+    const total = TotalSumOrders(totalOrdersDay)
+
+
+    return (
+        <Card className="flex flex-col justify-between p-5 max-sm:p-2 gap-2 h-32 max-sm:h-24">
+            <div className='flex justify-between items-center'>
+                <span className='opacity-75 max-md:text-sm'>Faturamento do dia</span>
+                <CircleDollarSign />
+            </div>
+            <h1 className='text-4xl max-md:text-2xl max-sm:text-base font-bold'>{formatReal(total)}</h1>
+        </Card>
+    )
+}
--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -5,6 +5,7 @@ import CardTotalOrders from './components/card-total-orders'
 import CardInvoicingTotal from './components/card-invoicing-total'
 import CardTotalUsers from './components/card-total-users'
 import { getProducts } from '@/actions/products'
+import CardTotalSalesDay from './components/card-totalsales-day'
 
 export default async function DashboardPage() {
 
@@ -12,13 +13,14 @@ export default async function DashboardPage() {
 
   return (
     <div className='flex  w-full h-full flex-col'>
-      <div className='grid grid-cols-3 gap-8 max-lg:gap-4 max-md:gap-2 w-full mb-8 max-sm:mb-4'>
+      <section className='grid grid-cols-2 sm:grid-cols-4 gap-8 max-lg:gap-4 max-md:gap-2 w-full mb-8 max-sm:mb-4'>
+        <CardTotalSalesDay />
         <CardTotalOrders />
         <CardInvoicingTotal />
         <CardTotalUsers />
-      </div>
+      </section>
 
-      <div className='flex max-lg:flex-col h-full gap-8 max-lg:gap-4 w-full'>
+      <section className='flex max-lg:flex-col h-full gap-8 max-lg:gap-4 w-full'>
         <Card className='p-5 w-full lg:max-w-sm  h-full min-h-fit'>
           <h2 className='text-lg font-bold'>Top Produtos</h2>
           <div className='w-full  gap-4 grid max-sm:gap-2 max-sm:grid-cols-1max-lg:grid-cols-2 mt-8 max-sm:mt-4 overflow-y-auto'>
@@ -32,7 +34,7 @@ export default async function DashboardPage() {
           <h2 className='text-lg font-bold'>Ultimos Pedidos</h2>
           <TableOrder />
         </div>
-      </div>
+      </section>
     </div>
   )
 }
