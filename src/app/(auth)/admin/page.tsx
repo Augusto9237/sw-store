998b356cb2b9e17651bcc91c61c69f1f55aa6c69
Augusto9237 <augusto.souza8330@gmail.com>
2024-08-27 00:29:51 -0300

    feat: update auth admin page

--- src/app/(auth)/admin/page.tsx
+++ src/app/(auth)/admin/page.tsx
@@ -1,12 +1,10 @@
-import AuthAdmin from "@/actions/auth"
-import { Button } from "@/components/ui/button"
 import { Card } from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
 import FormLogin from "./components/FormLogin"
+import Link from "next/link"
 
 export default function Admin() {
     return (
-        <div className="h-full min-h-full flex justify-center items-center absolute inset-0 bg-background/70 backdrop-blur-md">
+        <div className="h-full min-h-full flex flex-col gap-2 justify-center items-center absolute inset-0 bg-background/70 backdrop-blur-md">
             <Card className="flex flex-col justify-center text-center w-full max-w-[428px]  p-8 gap-2">
                 <h1 className="font-semibold text-lg md:text-2xl">
                     <span className="text-primary">Auto</span> Tech
@@ -15,6 +13,7 @@ export default function Admin() {
 
                 <FormLogin/>
             </Card >
+            <span >Não possui acesso? <Link href="/" className="font-semibold text-primary">Voltar para loja</Link></span>
         </div>
     )
 }
