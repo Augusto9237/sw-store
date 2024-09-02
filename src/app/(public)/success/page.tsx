6e34970d76f863975f957559abcb3c89e7290292
Augusto9237 <augusto.souza8330@gmail.com>
2024-09-02 01:32:27 -0300

    feat: add page success

--- src/app/(public)/success/page.tsx
+++ src/app/(public)/success/page.tsx
@@ -1,11 +1,16 @@
 
 export default async function Page() {
     const payment = await fetch(`${process.env.HOST_URL}/api/order/payment-success`, { method: 'POST' })
-    const res = await payment.json()
-    console.log(res)
+ 
+    if (payment.status === 400) {
+        // Trate o erro adequadamente, por exemplo, lançando uma exceção ou retornando um fallback
+        console.error(`Erro na requisição: ${payment.statusText}`);
+        return;
+    }
+    const data = await payment.json()
+    console.log(data)
     return (
         <ul>
-        
                 <p>
                     pagamento recebido
                 </p>
