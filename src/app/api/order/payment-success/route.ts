1c5539909a9ae7638e17eb59c58348bca4d99c49
Augusto9237 <augusto.souza8330@gmail.com>
2024-09-05 00:55:50 -0300

    feat: update

--- src/app/api/order/payment-success/route.ts
+++ src/app/api/order/payment-success/route.ts
@@ -8,49 +8,43 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 
 
 export async function POST(req: NextRequest) {
-    const signature = req.headers.get("Stripe-Signature");
+    const signature = req.headers.get("stripe-signature");
+
+    console.log("assinatura: ", signature);
+
     if (!signature) {
-        return NextResponse.json({ error: 'Assinatura Stripe ausente' }, { status: 400 });
+        return NextResponse.error();
     }
 
     const text = await req.text();
-    let event;
 
-    try {
-        event = stripe.webhooks.constructEvent(
-            text,
-            signature,
-            process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_SECRET_KEY,
-        );
-    } catch (err) {
-        console.error('Erro ao validar o webhook:', err);
-        return NextResponse.json({ error: 'Erro ao validar o webhook' }, { status: 400 });
-    }
+    const event = stripe.webhooks.constructEvent(
+        text,
+        signature,
+        process.env.NEXT_PUBLIC_STRIPE_WEBHOOK_SECRET_KEY,
+    );
 
     if (event.type === "checkout.session.completed") {
         const session = event.data.object as any;
 
-        if (!session.metadata.orderId) {
-            console.error('orderId não encontrado no metadata da sessão');
-            return NextResponse.json({ error: 'orderId não encontrado' }, { status: 400 });
-        }
+        const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
+            event.data.object.id,
+            {
+                expand: ["line_items"],
+            },
+        );
+        const lineItems = sessionWithLineItems.line_items;
 
-        try {
-            const sessionWithLineItems = await stripe.checkout.sessions.retrieve(
-                event.data.object.id,
-                { expand: ["line_items"] },
-            );
+        console.log(lineItems);
 
-            console.log(sessionWithLineItems.line_items);
-
-            await prismaClient.order.update({
-                where: { id: session.metadata.orderId },
-                data: { status: "PAYMENT_CONFIRMED" },
-            });
-        } catch (err) {
-            console.error('Erro ao processar a sessão do Stripe:', err);
-            return NextResponse.json({ error: 'Erro ao processar o pagamento' }, { status: 500 });
-        }
+        await prismaClient.order.update({
+            where: {
+                id: session.metadata.orderId,
+            },
+            data: {
+                status: "PAYMENT_CONFIRMED",
+            },
+        });
     }
 
     return NextResponse.json({ received: true });
