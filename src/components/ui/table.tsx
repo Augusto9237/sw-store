ce4202c235a40ab88af002d7869d2fce41deedc0
Augusto9237 <augusto.souza8330@gmail.com>
2024-09-14 22:02:33 -0300

    feat: update

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -6,7 +6,7 @@ const Table = React.forwardRef<
   HTMLTableElement,
   React.HTMLAttributes<HTMLTableElement>
 >(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
+  <div className="relative w-full overflow-auto [&::-webkit-scrollbar]:bg-background [&::-webkit-scrollbar]:w-[4px] [&::-webkit-scrollbar-thumb]:bg-foreground/30">
     <table
       ref={ref}
       className={cn("w-full caption-bottom text-sm", className)}
